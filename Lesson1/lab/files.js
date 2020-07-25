d9358feead6662fd2e1ffd49e0e75f288c428f13
DmPrkp <f89522795153@gmail.com>
2020-07-25 17:06:43 +0300

    lab is done

--- Lesson1/lab/files.js
+++ Lesson1/lab/files.js
@@ -4,42 +4,38 @@ const fs = require('fs');
 const header = "header.html";
 const body = "body.html";
 const footer = "footer.html";
+const textHtml = { 'Content-Type': 'text/html' };
 
-http.createServer((requrest, response)=> {
-    fs.readFile(header, 'utf8', (err, data)=>{
-        if(err) {
-            console.log('Could not find or open file for reading\n');
+
+http.createServer((requrest, response) => {
+
+    fs.readFile(header, 'utf8', (err, data) => {
+        if (err) {
             response.statusCode = 404;
             response.end();
         } else {
-            console.log(`The file ${header} is read and sent to the client\n`);
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
+            response.writeHead(200,textHtml);
+            response.write(data)
+            
+            fs.readFile(body, 'utf8', (err, data) => {
+                if (err) {
+                    response.statusCode = 404;
+                    response.end();
+                } else {
+                    response.write(data)
+
+                    fs.readFile(footer, 'utf8', (err, data) => {
+                        if (err) {
+                            response.statusCode = 404;
+                            response.end();
+                        } else {
+                            response.end(data);
+                        }
+                    });
+                }
+            });
         }
     });
 
-    fs.readFile(body, 'utf8', (err, data)=>{
-        if(err) {
-            console.log('Could not find or open file for reading\n');
-            response.statusCode = 404;
-            response.end();
-        } else {
-            console.log(`The file ${body} is read and sent to the client\n`);
-            response.writeHead(200, {'Content-Type':'text/html'});            
-            response.end(data);
-        }
-    });
 
-    fs.readFile(footer, 'utf8', (err, data)=>{
-        if(err) {
-            console.log('Could not find or open file for reading\n');
-            response.statusCode = 404;
-            response.end();
-        } else {
-            console.log(`The file ${footer} is read and sent to the client\n`);
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
-        }
-    });
-    
 }).listen(8080);
