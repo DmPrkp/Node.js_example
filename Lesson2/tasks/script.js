8db0d5d3eaffbeea6de636d98da9f813998e1c9a
DmPrkp <f89522795153@gmail.com>
2020-07-27 21:50:59 +0300

    lesson 2 task 2

--- Lesson2/tasks/script.js
+++ Lesson2/tasks/script.js
@@ -28,3 +28,36 @@ let newPassword = iteraGenerator(16)
 console.log(newPassword)
 
 // Task 2///////////////////////////////////////////////////////////
+
+let data2 = "data.txt";
+const fs = require('fs');
+
+fs.readFile(data2, 'utf8', (err, data) => {
+	if (err) {
+		console.log('Could not find or open file for reading\n');
+		response.statusCode = 404;
+		response.end();
+	} else {
+		let dataToArray = data.split(" ");
+		
+		let stringMultOf2 = dataToArray.map((num)=>{
+			 if (num%2 === 0) {
+				return num
+			};
+		}).join(" ");
+		console.log(`${stringMultOf2} -numbers multiple of 2`);
+		
+		let stringOfCub = dataToArray.map((num)=>{return Math.pow(num, 3)}).join(" ");
+		console.log(`${stringOfCub} -numbers of 3`);
+
+		fs.writeFile("out-1.txt", stringMultOf2, (err) => {
+			if (err) throw err;
+			console.log(`out-1.txt has been saved!`);
+		  });
+
+		fs.writeFile("out-2.txt", stringOfCub, (err) => {
+			if (err) throw err;
+			console.log(`out-2.txt has been saved!`);
+		  });
+	}
+});
