ea32dfe8908ccac3eec64ffee436ad84a0b3cc93
DmPrkp <f89522795153@gmail.com>
2020-07-31 23:29:14 +0300

    learn you node

--- Lesson4/lab/learnyounode/http-collect.js
+++ Lesson4/lab/learnyounode/http-collect.js
@@ -8,14 +8,3 @@ http.get(process.argv[2], (res)=>{
         console.log(data)
     })
 })
-
-////answer of autor////////////////////////////////////////////////////////////
-
-'use strict'
-const http = require('http')
-
-http.get(process.argv[2], function (response) {
-  response.setEncoding('utf8')
-  response.on('data', console.log)
-  response.on('error', console.error)
-}).on('error', console.error)
